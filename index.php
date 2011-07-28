4bfa82ffd0d7fe560712f0071d41691b50c1a594
Joseph McCullough <joseph@vertstudios.com>
2011-07-27 19:26:59 -0500

    Slideshow working but resizing not

--- index.php
+++ index.php
@@ -5,10 +5,42 @@
                 <div class="row">
                     <div class="twelvecol last">
                         <h1>Typo Demo Page (h1)</h1>
-                        <p>This is my boss, <a href="#">Jonathan Hart</a>, a self-made millionaire, he's quite a guy. This is Mrs H., she's gorgeous, she's one lady who knows how to take care of herself. By the way, my name is Max. I take care of both of them, which ain't easy, 'cause when they met it was MURDER!</p>
+						<p>
+							This is my boss, <a href="#">Jonathan Hart</a>, a self-made millionaire, 
+							he's quite a guy. This is Mrs H., she's gorgeous, she's one lady who knows 
+							how to take care of herself. By the way, my name is Max. 
+							I take care of both of them, which ain't easy, 'cause when they met it was MURDER!
+						</p>
                         <p><a href="#" class="button">Go</a></p>
+						<ol>
+							<li>Item 1</li>
+							<li>Item 2</li>
+							<li>Item 3</li>
+						</ol>
+						<ul>
+							<li>Item 1</li>
+							<li>Item 2</li>
+							<li>Item 3</li>
+						</ul>
                     </div>
-                </div><!-- end main row -->
+                </div><!--row-->
+				<div class="row">
+					<div class="twelvecol last">
+						<div id="slideshow_wrapper">
+							<div id="slideshow">
+								<div class="slide">
+									<img src="http://placehold.it/1110x400/ffff00">
+								</div>
+								<div class="slide jshidden">
+									<img src="http://placehold.it/1110x400/000054">
+								</div>
+								<div class="slide jshidden">
+									<img src="http://placehold.it/1110x400">
+								</div>
+							</div>
+						</div><!--slideshow_wrapper-->
+					</div>
+				</div><!--row-->
             </div><!-- end main container -->
             
         <?php require("php/footer.php"); ?>
--- js/script.js
+++ js/script.js
@@ -1,26 +1,20 @@
-/* Author: 
+/* Author: Joseph McCullough (@joe_query)
 
 */
 
+$(window).load(function(){
+	//$('#slideshow_wrapper').append('<div id="slideshow_nav"><ul><li class="slideNavActive">1</li><li>2</li><li>3</li></ul></div>');
+	
+	var slideshow = new Slaveshow("#slideshow",{
+		entrance: {speed: 500},
+		exit: {speed: 500},
+		duration: 3000,
+		transition: "fade",
+		nav: $("#slideNav li"),
+		left: $("#left"),
+		right: $("#right")
+	});
 
+	slideshow.animate();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}); //End jQuery
